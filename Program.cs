f97cb2d26b4b0e0506ae077d72818801e5312d37
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-06-16 21:45:07 -0300

    EXERCÍCIO CONSLUÍDO

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int numero;
+int resultado;
+
+Console.WriteLine("-=-Tabuada-=-");
+
+Console.WriteLine("Por favor, digite um número:");
+numero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Tabuada do {numero}");
+int numeroMultiplicador = 0;
+
+
+    while( numeroMultiplicador <= 10)
+        {
+            resultado = numero * numeroMultiplicador;
+            Console.WriteLine($"{numeroMultiplicador} x {numero} = {resultado}");
+            numeroMultiplicador +=1;        
+        }
+
+
+
